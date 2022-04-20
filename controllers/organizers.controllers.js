fd9346c2f169d3485d9c10a17a18a6bb256c514f
KachiiC <kacheokcheong@gmail.com>
2022-04-20 13:37:09 +0100

    added add organizers controller

--- controllers/organizers.controllers.js
+++ controllers/organizers.controllers.js
@@ -2,7 +2,9 @@ const Organizers = require("../models/organizers.models");
 
 const addOrganizer = async (ctx) => {
   try {
-    ctx.body = "add organizer";
+    const res = await ctx.request.body;
+    await Organizers.create(res)
+    ctx.body = res;
     ctx.status = 200;
   } catch (err) {
     ctx.status = 500;
