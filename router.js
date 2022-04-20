d220711cd1fe4cf8f3fd34ebcd637388c91e9249
KachiiC <kacheokcheong@gmail.com>
2022-04-20 11:43:10 +0100

    added attendees to router

--- router.js
+++ router.js
@@ -1,11 +1,15 @@
 const Router = require("koa-router");
+const { addAttendees, getAttendees } = require("./controllers/attendees.controllers");
 const { getEvents, addEvent } = require("./controllers/events.controllers");
 
 const router = new Router();
 
 router
   .get("", (ctx) => (ctx.body = "home route"))
-  //EVENTS
+  // ATTENDEES
+  .get("/attendees", getAttendees)
+  .post("/attendees", addAttendees)
+  // EVENTS
   .get("/events", getEvents)
   .post("/events", addEvent);
 
