f6dfca059cefb402da410fdd1a7202c6343dfbeb
KachiiC <kacheokcheong@gmail.com>
2022-04-21 00:43:17 +0100

    edit attendees and events controllers

--- controllers/attendees.controllers.js
+++ controllers/attendees.controllers.js
@@ -1,15 +1,35 @@
 const Attendees = require("../models/attendees.models");
+const Events = require("../models/events.models");
 
 const addAttendees = async (ctx) => {
+  // take the request body
+  const res = ctx.request.body;
+  // find _id that matches event id passed in url
+  const params = { _id: ctx.params.event_id };
   try {
-    // take the request body
-    const res = await ctx.request.body;
-    // create data on the Attendees collection
-    await Attendees.create(res);
-    // finds the data we just created and returns it as the body of response
-    const new_attendees = await Attendees.find({...res})
-    ctx.body = new_attendees;
-    ctx.status = 201;
+    const check = await Events.exists(params);
+    if (check === null) {
+      ctx.body = "event does not exist";
+      ctx.status = 404;
+    } else {
+      // find correct event
+      const event = await Events.findOne(params);
+
+      // help that create new attendees on input object that is passed
+      const creator = (input) => {
+        Attendees.create({
+          ...input,
+          event_name: event.event_name,
+          event_id: event._id,
+        });
+      };
+
+      // if body is array, create for each object in body, otherwise just create on body
+      Array.isArray(res) ? res.forEach((obj) => creator(obj)) : creator(res);
+      // return context of body
+      ctx.body = res;
+      ctx.status = 201;
+    }
   } catch (err) {
     ctx.status = 500;
     throw err;
@@ -17,10 +37,21 @@ const addAttendees = async (ctx) => {
 };
 
 const getAttendees = async (ctx) => {
+  // id from params
+  const req_id = ctx.params.event_id;
+
   try {
-    const res = await Attendees.find();
-    ctx.body = res;
-    ctx.status = 200;
+    const check = await Events.exists({ _id: req_id });
+
+    if (check == null) {
+      ctx.body = "event does not exist";
+      ctx.status = 404;
+    } else {
+      // gets attendees based on event_id passed in url
+      const res = await Attendees.find({ event_id: ctx.params.event_id });
+      ctx.body = res;
+      ctx.status = 200;
+    }
   } catch (err) {
     ctx.status = 500;
     throw err;
--- controllers/events.controllers.js
+++ controllers/events.controllers.js
@@ -1,15 +1,49 @@
+const Attendees = require("../models/attendees.models");
 const Events = require("../models/events.models");
+const Organizers = require("../models/organizers.models");
 
 const addEvent = async (ctx) => {
+  // id from params
+  const req_id = ctx.params.organizer_id;
+  // input body
+  const input_body = ctx.request.body;
   try {
-    // take the request body
-    const res = await ctx.request.body;
-    // create data on the Events collection
-    await Events.create(res);
-    // finds the data we just created and returns it as the body of response
-    const db_data = await Events.findOne({ ...res });
-    ctx.body = db_data;
-    ctx.status = 201;
+    const check = await Organizers.exists({ email: req_id });
+    if (check === null) {
+      ctx.body = "user does not exist";
+      ctx.status = 404;
+    } else {
+      await Events.create(input_body);
+      ctx.body = await Events.findOne(input_body);
+      ctx.status = 201;
+    }
+    // // finds the data we just created and returns it as the body of response
+  } catch (err) {
+    ctx.status = 500;
+    throw err;
+  }
+};
+
+const getEvent = async (ctx) => {
+  // event_id passed through url
+  const input_id = ctx.params.event_id;
+  // check if id is same as input event_id
+  const _idArg = { _id: input_id };
+  try {
+    const check = await Events.exists(_idArg);
+
+    if (check === null) {
+      ctx.body = "user does not exist";
+      ctx.status = 404;
+    } else {
+      // count total number of attendees for event
+      const total = await Attendees.countDocuments({ event_id: input_id });
+      // update event attendees number
+      await Events.findOneAndUpdate(_idArg, { attendees: total });
+      // return the event
+      ctx.body = await Events.findOne(_idArg);
+      ctx.status = 200;
+    }
   } catch (err) {
     ctx.status = 500;
     throw err;
@@ -18,4 +52,5 @@ const addEvent = async (ctx) => {
 
 module.exports = {
   addEvent,
+  getEvent,
 };
