147ed746cc87d303af145554a7ccad7fa5b050a2
KachiiC <kacheokcheong@gmail.com>
2022-04-20 11:20:02 +0100

    added index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+const Koa = require("koa");
+const koa_parser = require("koa-bodyparser");
+const koa_cors = require("@koa/cors");
+const app = new Koa();
+const port = 8000;
+
+app.use(koa_parser())
+.use(koa_cors());
+
+app.listen(port, () => {
+  console.log(`🚀 Server listening http://127.0.0.1:${port}/ 🚀`);
+});
