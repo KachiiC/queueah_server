e23fd00536204db2dd762979f2d7f9ee6d7342ac
KachiiC <kacheokcheong@gmail.com>
2022-04-20 11:39:03 +0100

    added events to routes

--- router.js
+++ router.js
@@ -1,7 +1,12 @@
 const Router = require("koa-router");
+const { getEvents, addEvent } = require("./controllers/events.controllers");
 
 const router = new Router();
 
-router.get("", (ctx) => ctx.body = "home route");
+router
+  .get("", (ctx) => (ctx.body = "home route"))
+  //EVENTS
+  .get("/events", getEvents)
+  .post("/events", addEvent);
 
 module.exports = router;
