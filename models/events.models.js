20016fe97905f5342096dfe4d283c8ba35ec70b0
KachiiC <kacheokcheong@gmail.com>
2022-04-21 12:07:48 +0100

    added events admitted logic to events

--- models/events.models.js
+++ models/events.models.js
@@ -4,11 +4,13 @@ const { urlTypes } = require("../helpers/validate.helpers");
 
 const eventsSchema = new Schema({
   address: String,
+  admitted: Number,
   adults_only: Boolean,
   attendees: { type: Number, default: 0 },
   date: Date,
   description: String,
   event_name: String,
+  not_admitted: Number,
   organizer: String,
   venue_name: String,
   website: urlTypes,
